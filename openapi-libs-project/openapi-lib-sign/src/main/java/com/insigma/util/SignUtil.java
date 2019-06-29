654b3f7266f1fade430d1b34558c86ed9313e0a3
hatemyselfandyouall <aaaasssswwwwdsaw@163.com>
2019-06-29 10:26:00 +0800

    关闭验证

--- openapi-libs-project/openapi-lib-sign/src/main/java/com/insigma/util/SignUtil.java
+++ openapi-libs-project/openapi-lib-sign/src/main/java/com/insigma/util/SignUtil.java
@@ -66,34 +66,36 @@ public class SignUtil {
      */
     public static JSONObject checkSign(JSONObject params,String appSecret){
         JSONObject result = new JSONObject();
-        try {
-            JSONObject checkParamResult = checkParamsVaild(params);
-            if (checkParamResult.getInteger("flag") != 1) {
-                return checkParamResult;
-            }
-            result.put("flag", 0);
-            String signature = params.getString("signature");
-            if (StringUtil.isEmpty(signature)) {
-                result.put("msg", "签名signature异常");
-            } else {
-                params.remove("signature");
-            }
-            String signModel=createSign(params,appSecret);
-            SortedMap testMap= new ConcurrentSkipListMap(params);
-            String signOld=createSign(testMap,appSecret);
-            params.put("signature",signModel);//todo testValue
-            System.out.println(params);//todo testValue
-            if (!signature.equals(signModel)&&!signature.equals(signOld)) {
-                result.put("msg", "参数与生成规则不符");
-            } else {
-                result.put("msg", "验证通过");
-                result.put("flag", 1);
-            }
-        }catch (Exception e){
-            result.put("flag",0);
-            result.put("msg","验证参数异常！");
-            log.error("验证参数异常",e);
-        }
+//        try {
+//            JSONObject checkParamResult = checkParamsVaild(params);
+//            if (checkParamResult.getInteger("flag") != 1) {
+//                return checkParamResult;
+//            }
+//            result.put("flag", 0);
+//            String signature = params.getString("signature");
+//            if (StringUtil.isEmpty(signature)) {
+//                result.put("msg", "签名signature异常");
+//            } else {
+//                params.remove("signature");
+//            }
+//            String signModel=createSign(params,appSecret);
+//            SortedMap testMap= new ConcurrentSkipListMap(params);
+//            String signOld=createSign(testMap,appSecret);
+//            params.put("signature",signModel);//todo testValue
+//            System.out.println(params);//todo testValue
+//            if (!signature.equals(signModel)&&!signature.equals(signOld)) {
+//                result.put("msg", "参数与生成规则不符");
+//            } else {
+//                result.put("msg", "验证通过");
+//                result.put("flag", 1);
+//            }
+//        }catch (Exception e){
+//            result.put("flag",0);
+//            result.put("msg","验证参数异常！");
+//            log.error("验证参数异常",e);
+//        }
+        result.put("msg", "验证通过");
+        result.put("flag", 1);
         return result;
     }
 
@@ -124,8 +126,8 @@ public class SignUtil {
 
 
     public static void main(String[] args) throws IOException {
-        String testSecret="5cf7bfa05c72443a88aa3f7c53793570";
-        String testKey="dba93607e34641e184c70c7a04ab91bd";
+        String testKey="test6";
+        String testSecret="test";
         JSONObject haeder=new JSONObject();
         haeder.put("appKey",testKey);
         haeder.put("time", "20190628 11:44:07");
@@ -145,7 +147,7 @@ public class SignUtil {
 //        System.out.println(checkSign(paramJson,testSecret));
 //        paramJson=getParamWithoutsignatureParam(param);
 //        haeder.put("signature",signature);
-        String testUrl="http://localhost:10500/frontInterface/interface/transerService-7102";
+        String testUrl="http://10.87.0.68/api/frontInterface/interface/doCallBack-7122";
         postTest(haeder,param,testUrl);
     }
 
