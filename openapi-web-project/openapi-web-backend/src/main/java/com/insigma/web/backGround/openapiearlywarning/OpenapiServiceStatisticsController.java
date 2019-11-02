009c1a829edf0cacb2d7e42d30de0534a5eda6a9
gaozd <gaozhengde1993@gmai.com>
2019-11-02 16:05:51 +0800

    结构调整

--- openapi-web-project/openapi-web-backend/src/main/java/com/insigma/web/backGround/openapiearlywarning/OpenapiServiceStatisticsController.java
+++ openapi-web-project/openapi-web-backend/src/main/java/com/insigma/web/backGround/openapiearlywarning/OpenapiServiceStatisticsController.java
@@ -10,6 +10,7 @@ import com.insigma.facade.openapi.facade.OpenapiEarlyWarningFacade;
 import com.insigma.facade.openapi.po.OpenapiApp;
 import com.insigma.facade.openapi.po.OpenapiDictionary;
 import com.insigma.facade.openapi.po.OpenapiEarlyWarning;
+import com.insigma.util.BIUtil;
 import com.insigma.util.JSONUtil;
 import com.insigma.web.BasicController;
 import constant.DataConstant;
@@ -28,7 +29,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/openapiServiceStatistic")
-@Api(tags ="BI接口转发接口")
+@Api(tags = "BI接口转发接口")
 @Slf4j
 public class OpenapiServiceStatisticsController extends BasicController {
 
@@ -40,33 +41,14 @@ public class OpenapiServiceStatisticsController extends BasicController {
 
     @ApiOperation(value = "BI接口转发接口")
     @ResponseBody
-    @RequestMapping(value = "/InterfaceCallTrend/{code}",method = RequestMethod.GET,produces = {"application/json;charset=UTF-8"})
-    public ResultVo<String> getInterfaceCallTrend( @RequestParam(value = "pageNum", required = false) Long pageNum,
-                                                   @RequestParam(value = "pageSize", required = false) Long pageSize,
-                                                   @RequestParam(value = "whereWord", required = false) String whereWord,
-                                                   @RequestParam(value = "orderByWord", required = false) String orderByWord,
-                                                   @PathVariable String code){
-        ResultVo resultVo=new ResultVo();
-        try {
-            String testUrl=openapiDictionaryFacade.getValueByCode(code);
-            Map<String,String> stringStringMap=new HashMap<>();
-            stringStringMap.put("pageNum",pageNum+"");
-            stringStringMap.put("pageSize",pageSize+"");
-            stringStringMap.put("whereWord",whereWord+"");
-            stringStringMap.put("orderByWord",orderByWord+"");
-            ResponseEntity<String> result= RestTemplateConfig.getWithParamterMap(testUrl,stringStringMap,restTemplate);
-            if (result.getStatusCode().is2xxSuccessful()){
-                resultVo.setResult(JSONObject.parseObject(result.getBody()));
-            }else {
-                resultVo.setResultDes(result.getBody());
-            }
-        }catch (Exception e){
-            resultVo.setResultDes("接口调用趋势异常");
-            log.error("接口调用趋势异常",e);
-        }
-        return resultVo;
+    @RequestMapping(value = "/InterfaceCallTrend/{code}", method = RequestMethod.GET, produces = {"application/json;charset=UTF-8"})
+    public ResultVo<String> getInterfaceCallTrend(@RequestParam(value = "pageNum", required = false) Long pageNum,
+                                                  @RequestParam(value = "pageSize", required = false) Long pageSize,
+                                                  @RequestParam(value = "whereWord", required = false) String whereWord,
+                                                  @RequestParam(value = "orderByWord", required = false) String orderByWord,
+                                                  @PathVariable String code) {
+        return BIUtil.getRequestResult(pageNum, pageSize, whereWord, orderByWord, code, openapiDictionaryFacade, restTemplate);
     }
-
 
 
 }
