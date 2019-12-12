aac5ddaa70dee21e2e61bf7f0a2d4a59e0d744e4
gaozd <gaozhengde1993@gmai.com>
2019-12-12 10:48:16 +0800

    调整

--- openapi-web-project/openapi-web-backend/src/main/java/com/insigma/web/backGround/user/SysOrgController.java
+++ openapi-web-project/openapi-web-backend/src/main/java/com/insigma/web/backGround/user/SysOrgController.java
@@ -5,16 +5,19 @@ import com.insigma.facade.sysbase.SysOrgFacade;
 import com.insigma.facade.sysbase.SysUserFacade;
 import com.insigma.facade.sysbase.vo.SysOrgDTO;
 import com.insigma.facade.sysbase.vo.SysUserDTO;
+import com.insigma.facade.sysbase.vo.SysUserOrgDTO;
 import com.insigma.webtool.component.LoginComponent;
 import com.insigma.webtool.util.TreeUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import star.bizbase.exception.BizRuleException;
 import star.bizbase.vo.result.Results;
 import star.vo.result.ResultVo;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("SysOrg")
@@ -60,5 +63,53 @@ public class SysOrgController {
         result.setSuccess(true);
         result.setResult(orgList);
         return result;
+    }
+
+    /**
+     * 查询树
+     * @return
+     * @throws BizRuleException
+     */
+    @ResponseBody
+    @RequestMapping(value = { "/queryTree" })
+    public ResultVo<JSONArray> queryTree() throws BizRuleException {
+        //获取当前登录用户的所属区域
+        //从缓存获取用户信息
+        Long userId = loginComponent.getLoginUserId();
+        SysUserDTO user = sysUserFacade.getCacheByPrimaryKey(userId);
+        ResultVo<JSONArray> result = Results.newResultVo();
+        List<SysOrgDTO> orgList = new ArrayList<>();
+        if(user.getUserType().equals("1")){//管理员获取所有机构
+            HashMap<String, Object> searchMap = new HashMap<>();
+            searchMap.put("orgState", "1");
+            orgList = sysOrgFacade.getListByWhere(searchMap);
+        }else{
+//			orgList = sysOrgFacade.queryOrgNodes(user.getAreaId().toString());
+            List<SysUserOrgDTO> userOrgList = sysUserFacade.queryUserOrg(userId);
+            if(userOrgList.size()>0){
+                Set<String> s = new HashSet<>();
+                for(SysUserOrgDTO uo :userOrgList){
+                    SysOrgDTO org = sysOrgFacade.getByPrimaryKey(uo.getOrgId());
+                    String idpath = org.getIdpath();
+                    String[] splitAddress=idpath.split("/"); //如果以竖线为分隔符，则split的时候需要加上两个斜杠【\\】进行转义
+                    for(int i=0;i<splitAddress.length;i++){
+                        s.add(splitAddress[i]);
+                    }
+                }
+                String orgIds = "'" + StringUtils.join(s, "','") + "'";
+                orgList = sysOrgFacade.queryOrgListByOrgIds(orgIds);
+            }
+        }
+        JSONArray jsonArray = null;
+        if(orgList.size()>0){
+            jsonArray = TreeUtil.listToTree(JSONArray.parseArray(JSONArray.toJSONString(orgList)), "id",
+                    "parentId", "children");
+        }
+
+        result.setCode("0");
+        result.setSuccess(true);
+        result.setResult(jsonArray);
+        return result;
+
     }
 }
