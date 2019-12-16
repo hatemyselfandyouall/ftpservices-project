3eb58c67c74bf17e08e5003d80847781a4df35b8
gaozd <gaozhengde1993@gmai.com>
2019-12-16 16:41:03 +0800

    调整

--- openapi-web-project/openapi-web-backend/src/main/java/com/insigma/webtool/interceptor/PermissionInterceptor.java
+++ openapi-web-project/openapi-web-backend/src/main/java/com/insigma/webtool/interceptor/PermissionInterceptor.java
@@ -3,18 +3,20 @@ package com.insigma.webtool.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.insigma.facade.sysbase.SysUserFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.insigma.webtool.component.LoginComponent;
+import star.bizbase.exception.BizRuleException;
 
 public class PermissionInterceptor extends HandlerInterceptorAdapter {
 	@Autowired
 	private LoginComponent loginComonent;
-	
-//	@Autowired
-//	private PurviewFacade purviewFacade;
+
+	@Autowired
+	private SysUserFacade sysUserFacade;
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -23,11 +25,11 @@ public class PermissionInterceptor extends HandlerInterceptorAdapter {
 			request.getRequestDispatcher("/index.html").forward(request, response);
 			return false;
 		}
-		
-//		int userId = loginComonent.getLoginUserId();
-//		if (!purviewFacade.getAuthByUrl(userId, uri)) {
-//			throw new PermissionException("权限不足");
-//		}
+
+		Long userId = loginComonent.getLoginUserId();
+		if (!sysUserFacade.getAuthByUrlAndUserId(uri, userId)) {
+			throw new BizRuleException("权限不足");
+		}
 		
 		return super.preHandle(request, response, handler);
 	}
