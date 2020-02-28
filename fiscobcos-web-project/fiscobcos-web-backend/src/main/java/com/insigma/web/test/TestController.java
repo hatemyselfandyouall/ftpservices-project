b4f0c384c02ecde2b68d9e9a34b9b78aab8c3836
gaozd <gaozhengde1993@gmai.com>
2020-02-28 10:55:19 +0800

    切成203试试

--- fiscobcos-web-project/fiscobcos-web-backend/src/main/java/com/insigma/web/test/TestController.java
+++ fiscobcos-web-project/fiscobcos-web-backend/src/main/java/com/insigma/web/test/TestController.java
@@ -1,11 +1,15 @@
 package com.insigma.web.test;
 
+import com.insigma.contract.Asset;
 import lombok.extern.slf4j.Slf4j;
 import org.fisco.bcos.channel.client.Service;
 import org.fisco.bcos.web3j.crypto.Credentials;
 import org.fisco.bcos.web3j.crypto.Keys;
 import org.fisco.bcos.web3j.protocol.Web3j;
 import org.fisco.bcos.web3j.protocol.channel.ChannelEthereumService;
+import org.fisco.bcos.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.fisco.bcos.web3j.tuples.generated.Tuple2;
+import org.fisco.bcos.web3j.tx.gas.StaticGasProvider;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -13,7 +17,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.UUID;
+import java.math.BigInteger;
 
 @Controller
 @RequestMapping("/test")
@@ -31,16 +35,19 @@ public class TestController {
 //            redisTemplate.opsForValue().set(i+"","缓存测试"+i);
 //        }
 		try {
-			ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-			Service service = context.getBean(Service.class);
-			service.run();
-
-			ChannelEthereumService channelEthereumService = new ChannelEthereumService();
-			channelEthereumService.setChannelService(service);
-// 初始化Web3j对象
-			Web3j web3j = Web3j.build(channelEthereumService, 1);
-// 初始化Credentials对象
-			Credentials credentials = Credentials.create(Keys.createEcKeyPair());
+            ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+            Service service = context.getBean(Service.class);
+            service.run();
+            ChannelEthereumService channelEthereumService = new ChannelEthereumService();
+            channelEthereumService.setChannelService(service);
+            channelEthereumService.setTimeout(10000);
+            Web3j web3j = Web3j.build(channelEthereumService, service.getGroupId());
+            Credentials credentials = Credentials.create(Keys.createEcKeyPair());
+            //准备部署和调用合约的参数
+            Asset asset = Asset.deploy(web3j, credentials, new StaticGasProvider(new BigInteger("1"), new BigInteger("10000"))).send();
+            Tuple2<BigInteger, BigInteger> result = asset.select("1").send();
+// register接口调用
+            TransactionReceipt receipt = asset.register("1", new BigInteger("1")).send();
 		}catch (Exception e){
 			log.error("测试区块链异常",e);
 			return "测试区块链异常"+e;
@@ -49,8 +56,19 @@ public class TestController {
 	}
 
 	public static void main(String[] args) {
-		for (int i=0;i<=3;i++){
-			System.out.println(UUID.randomUUID().toString().replaceAll("-",""));
-		}
+        try {
+            ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+            Service service = context.getBean(Service.class);
+            service.run();
+
+            ChannelEthereumService channelEthereumService = new ChannelEthereumService();
+            channelEthereumService.setChannelService(service);
+// 初始化Web3j对象
+            Web3j web3j = Web3j.build(channelEthereumService, 1);
+// 初始化Credentials对象
+            Credentials credentials = Credentials.create(Keys.createEcKeyPair());
+        }catch (Exception e){
+            log.error("测试区块链异常",e);
+        }
 	}
 }
