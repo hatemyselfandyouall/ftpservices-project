8acda3084489cc69c671cc8411f504af37e79fc6
gaozd <gaozhengde1993@gmai.com>
2020-03-18 15:55:33 +0800

    init

--- qrcodebase-libs-project/qrcodebase-lib-base/src/main/java/com/insigma/util/QRCodeUtil.java
+++ qrcodebase-libs-project/qrcodebase-lib-base/src/main/java/com/insigma/util/QRCodeUtil.java
@@ -1,4 +1,32 @@
 package com.insigma.util;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import javax.imageio.ImageIO;
+import io.nayuki.qrcodegen.*;
+
 public class QRCodeUtil {
+    public static void main(String[] args) throws IOException {
+        QrCode qr0 = QrCode.encodeText("Hello, world!", QrCode.Ecc.MEDIUM);
+        BufferedImage img = qr0.toImage(4, 10);
+        ImageIO.write(img, "png", new File("qr-code.png"));
+
+// Manual operation
+        List<QrSegment> segs = QrSegment.makeSegments("3141592653589793238462643383");
+//        QrCode qr1 = QrCode.encodeSegments(segs, QrCode.Ecc.HIGH, 5, 5, 2, false);
+//        for (int y = 0; y < qr1.size; y++) {
+//            for (int x = 0; x < qr1.size; x++) {
+//                (... paint qr1.getModule(x, y) ...)
+//            }
+//        }
+    }
+
+    public static BufferedImage toQrImage(String text) throws IOException {
+        QrCode qr0 = QrCode.encodeText(text, QrCode.Ecc.MEDIUM);
+        BufferedImage img = qr0.toImage(4, 10);
+//        ImageIO.write(img, "png", new File("qr-code.png"));
+        return img;
+    }
 }
--- qrcodebase-web-project/qrcodebase-web-backend/src/main/java/com/insigma/web/backGround/QRCodeController.java
+++ qrcodebase-web-project/qrcodebase-web-backend/src/main/java/com/insigma/web/backGround/QRCodeController.java
@@ -1,4 +1,36 @@
 package com.insigma.web.backGround;
 
+
+import com.insigma.util.QRCodeUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import star.vo.result.ResultVo;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.OutputStream;
+
+@RestController
+@RequestMapping("qrcode")
+@Api(tags = "二维码模块")
+@Slf4j
 public class QRCodeController {
+
+    @ApiOperation(value = "二维码模块")
+    @RequestMapping(value = "/textToIMG",produces = {"application/json;charset=UTF-8"})
+    public void textToIMG(String text, HttpServletResponse response){
+//        ResultVo resultVo=new ResultVo();
+        try(OutputStream os=response.getOutputStream()){
+            BufferedImage bufferedImage=QRCodeUtil.toQrImage(text);
+            ImageIO.write(bufferedImage, "PNG",os);
+        }catch (Exception e){
+            log.error("二维码生成异常",e);
+        }
+//        return resultVo;
+    }
 }
