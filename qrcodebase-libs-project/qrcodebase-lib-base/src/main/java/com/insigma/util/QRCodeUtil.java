191a0da59123470f4b1ba8b15000880af6e851d6
gaozd <gaozhengde1993@gmai.com>
2020-03-19 10:13:52 +0800

    支持变色与图片的二维码生成工具

--- qrcodebase-libs-project/qrcodebase-lib-base/src/main/java/com/insigma/util/QRCodeUtil.java
+++ qrcodebase-libs-project/qrcodebase-lib-base/src/main/java/com/insigma/util/QRCodeUtil.java
@@ -1,32 +1,185 @@
 package com.insigma.util;
 
+import com.google.zxing.*;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import io.swagger.models.auth.In;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import javax.imageio.ImageIO;
-import io.nayuki.qrcodegen.*;
+import java.io.OutputStream;
+import java.util.Hashtable;
 
 public class QRCodeUtil {
-    public static void main(String[] args) throws IOException {
-        QrCode qr0 = QrCode.encodeText("Hello, world!", QrCode.Ecc.MEDIUM);
-        BufferedImage img = qr0.toImage(4, 10);
-        ImageIO.write(img, "png", new File("qr-code.png"));
 
-// Manual operation
-        List<QrSegment> segs = QrSegment.makeSegments("3141592653589793238462643383");
-//        QrCode qr1 = QrCode.encodeSegments(segs, QrCode.Ecc.HIGH, 5, 5, 2, false);
-//        for (int y = 0; y < qr1.size; y++) {
-//            for (int x = 0; x < qr1.size; x++) {
-//                (... paint qr1.getModule(x, y) ...)
-//            }
-//        }
+
+    private static final String CHARSET = "utf-8";
+    private static final String FORMAT_NAME = "JPG";
+    // 二维码尺寸
+    private static final int QRCODE_SIZE = 300;
+    // LOGO宽度
+    private static final int WIDTH = 60;
+    // LOGO高度
+    private static final int HEIGHT = 60;
+
+    private static BufferedImage createImage(String content, String imgPath, boolean needCompress, Integer color) throws Exception{
+        Hashtable hints = new Hashtable();
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        hints.put(EncodeHintType.CHARACTER_SET, CHARSET);
+        hints.put(EncodeHintType.MARGIN, 1);
+        BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, QRCODE_SIZE, QRCODE_SIZE,
+                hints);
+        int width = bitMatrix.getWidth();
+        int height = bitMatrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, bitMatrix.get(x, y) ? color : 0xFFFFFFFF);
+            }
+        }
+        if (imgPath == null || "".equals(imgPath)) {
+            return image;
+        }
+        // 插入图片
+        QRCodeUtil.insertImage(image, imgPath, needCompress);
+        return image;
     }
 
-    public static BufferedImage toQrImage(String text) throws IOException {
-        QrCode qr0 = QrCode.encodeText(text, QrCode.Ecc.MEDIUM);
-        BufferedImage img = qr0.toImage(4, 10);
-//        ImageIO.write(img, "png", new File("qr-code.png"));
-        return img;
+    private static BufferedImage createImage(String content, String imgPath, boolean needCompress) throws Exception {
+        return createImage(content,imgPath,needCompress,0xFF000000);
+    }
+
+    private static void insertImage(BufferedImage source, String imgPath, boolean needCompress) throws Exception {
+        File file = new File(imgPath);
+        if (!file.exists()) {
+            System.err.println("" + imgPath + "   该文件不存在！");
+            return;
+        }
+        Image src = ImageIO.read(new File(imgPath));
+        int width = src.getWidth(null);
+        int height = src.getHeight(null);
+        if (needCompress) { // 压缩LOGO
+            if (width > WIDTH) {
+                width = WIDTH;
+            }
+            if (height > HEIGHT) {
+                height = HEIGHT;
+            }
+            Image image = src.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            BufferedImage tag = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            Graphics g = tag.getGraphics();
+            g.drawImage(image, 0, 0, null); // 绘制缩小后的图
+            g.dispose();
+            src = image;
+        }
+        // 插入LOGO
+        Graphics2D graph = source.createGraphics();
+        int x = (QRCODE_SIZE - width) / 2;
+        int y = (QRCODE_SIZE - height) / 2;
+        graph.drawImage(src, x, y, width, height, null);
+        Shape shape = new RoundRectangle2D.Float(x, y, width, width, 6, 6);
+        graph.setStroke(new BasicStroke(3f));
+        graph.draw(shape);
+        graph.dispose();
+    }
+
+    public static void encode(String content, String imgPath, String destPath, boolean needCompress) throws Exception {
+        BufferedImage image = QRCodeUtil.createImage(content, imgPath, needCompress);
+        mkdirs(destPath);
+        // String file = new Random().nextInt(99999999)+".jpg";
+        // ImageIO.write(image, FORMAT_NAME, new File(destPath+"/"+file));
+        ImageIO.write(image, FORMAT_NAME, new File(destPath));
+    }
+
+    public static void encode(String content, String imgPath, String destPath, boolean needCompress, Integer color) throws Exception {
+        BufferedImage image = QRCodeUtil.createImage(content, imgPath, needCompress,color);
+        mkdirs(destPath);
+        // String file = new Random().nextInt(99999999)+".jpg";
+        // ImageIO.write(image, FORMAT_NAME, new File(destPath+"/"+file));
+        ImageIO.write(image, FORMAT_NAME, new File(destPath));
+    }
+
+
+
+    public static BufferedImage encode(String content, String imgPath, boolean needCompress) throws Exception {
+        BufferedImage image = QRCodeUtil.createImage(content, imgPath, needCompress);
+        return image;
+    }
+
+    public static void mkdirs(String destPath) {
+        File file = new File(destPath);
+        // 当文件夹不存在时，mkdirs会自动创建多层目录，区别于mkdir．(mkdir如果父目录不存在则会抛出异常)
+        if (!file.exists() && !file.isDirectory()) {
+            file.mkdirs();
+        }
+    }
+
+    public static void encode(String content, String imgPath, String destPath) throws Exception {
+        QRCodeUtil.encode(content, imgPath, destPath, false);
+    }
+    // 被注释的方法
+    /*
+     * public static void encode(String content, String destPath, boolean
+     * needCompress) throws Exception { QRCodeUtil.encode(content, null, destPath,
+     * needCompress); }
+     */
+
+    public static void encode(String content, String destPath) throws Exception {
+        QRCodeUtil.encode(content, null, destPath, false);
+    }
+
+    public static void encode(String content, String imgPath, OutputStream output, boolean needCompress)
+            throws Exception {
+        BufferedImage image = QRCodeUtil.createImage(content, imgPath, needCompress);
+        ImageIO.write(image, FORMAT_NAME, output);
+    }
+
+    public static void encode(String content, OutputStream output) throws Exception {
+        QRCodeUtil.encode(content, null, output, false);
+    }
+
+    public static String decode(File file) throws Exception {
+        BufferedImage image;
+        image = ImageIO.read(file);
+        if (image == null) {
+            return null;
+        }
+        BufferedImageLuminanceSource source = new BufferedImageLuminanceSource(image);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        Result result;
+        Hashtable hints = new Hashtable();
+        hints.put(DecodeHintType.CHARACTER_SET, CHARSET);
+        result = new MultiFormatReader().decode(bitmap, hints);
+        String resultStr = result.getText();
+        return resultStr;
+    }
+
+    public static String decode(String path) throws Exception {
+        return QRCodeUtil.decode(new File(path));
+    }
+
+
+
+    public static void main(String[] args) throws Exception{
+        createQRCodeWithColorAndImg("{\"idcard\":\"330682198811025923\",\"sjgb\":\"1\",\"model\":\"1\",\"name\":\"丁琪琦\",\"month\":\"12\",\"trade\":\"7666\"}\"","F:\\test0.jpg",0xFF228B22);
+    }
+
+    public static void createQRCodeWithColorAndImg(String text,String imgPath,Integer color)throws Exception{
+//        String text = "二维码还挺好玩的";
+        // 嵌入二维码的图片路径
+//        String imgPath = "F:\\zhaohuanc.jpg";
+        // 生成的二维码的路径及名称
+        String destPath = "F:\\test1.jpg";
+        //生成二维码
+        QRCodeUtil.encode(text, imgPath, destPath, true,0xFF228B22);
+
+        // 解析二维码
+        String str = QRCodeUtil.decode(destPath);
+        // 打印出解析出的内容
+        System.out.println(str);
     }
 }
