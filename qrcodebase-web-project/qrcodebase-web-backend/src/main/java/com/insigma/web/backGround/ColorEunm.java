d853a3cf09da35179b2e9080d4a7b55ad9537e14
gaozd <gaozhengde1993@gmai.com>
2020-03-26 11:56:06 +0800

    四色支持

--- qrcodebase-web-project/qrcodebase-web-backend/src/main/java/com/insigma/web/backGround/ColorEunm.java
+++ qrcodebase-web-project/qrcodebase-web-backend/src/main/java/com/insigma/web/backGround/ColorEunm.java
@@ -2,7 +2,7 @@ package com.insigma.web.backGround;
 
 public enum ColorEunm {
 
-    GREEN(0x50B061),RED(0xFF4B1A);
+    GREEN(0x50B061),RED(0xFF4B1A),YELLOW(0xFFC216),BLACK(0x8A8A8A);
 
     private Integer colerInt;
 
